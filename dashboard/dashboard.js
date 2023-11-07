92a5bd178258c1fa0ac95057af2acf61d8384a34
Kaushalpohekar <Kaushalpohekar1@gmail.com>
2023-11-07 13:39:37 +0530

    Kaushal

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -234,71 +234,9 @@ function getDataByTimeInterval(req, res) {
       return res.status(400).json({ message: 'Invalid time interval' });
     }
 
-    let duration;
-    switch (timeInterval) {
-      case '30sec':
-        duration = 'INTERVAL 30 SECOND';
-        break;
-      case '1min':
-        duration = 'INTERVAL 1 MINUTE';
-        break;
-      case '2min':
-        duration = 'INTERVAL 2 MINUTE';
-        break;
-      case '5min':
-        duration = 'INTERVAL 5 MINUTE';
-        break;
-      case '10min':
-        duration = 'INTERVAL 10 MINUTE';
-        break;
-      case '30min':
-        duration = 'INTERVAL 30 MINUTE';
-        break;
-      case '1hour':
-        duration = 'INTERVAL 1 HOUR';
-        break;
-      case '2hour':
-        duration = 'INTERVAL 2 HOUR';
-        break;
-      case '10hour':
-        duration = 'INTERVAL 10 HOUR';
-        break;
-      case '12hour':
-        duration = 'INTERVAL 12 HOUR';
-        break;
-      case '1day':
-        duration = 'INTERVAL 1 DAY';
-        break;
-      case '7day':
-        duration = 'INTERVAL 7 DAY';
-        break;
-      case '30day':
-        duration = 'INTERVAL 30 DAY';
-        break;
-      default:
-        return res.status(400).json({ message: 'Invalid time interval' });
-    }
-
-    const sql = `SELECT * FROM ORP_Meter WHERE DeviceUID = ? AND TimeStamp >= DATE_SUB(NOW(), ${duration})`;
-    db.query(sql, [deviceId], (error, results) => {
-      if (error) {
-        console.error('Error fetching data:', error);
-        return res.status(500).json({ message: 'Internal server error' });
-      }
-      res.json({ data: results });
-    });
-  } catch (error) {
-    console.error('An error occurred:', error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-}
-function getDataByTimeInterval2(req, res) {
-  try {
-    const deviceId = req.params.deviceId;
-    const timeInterval = req.query.interval;
-    if (!timeInterval) {
-      return res.status(400).json({ message: 'Invalid time interval' });
-    }
+    // Define your start_time and end_time here
+    const start_time = new Date('your_start_time_here');
+    const end_time = new Date('your_end_time_here');
 
     let duration;
     switch (timeInterval) {
@@ -351,13 +289,61 @@ function getDataByTimeInterval2(req, res) {
         console.error('Error fetching data:', error);
         return res.status(500).json({ message: 'Internal server error' });
       }
-      res.json({ data: results });
+
+      let pump1OnTime = 0;
+      let pump2OnTime = 0;
+      let combinedOfflineTime = 0;
+
+      let prevPump1State = 0;
+      let prevPump2State = 0;
+      let prevDateTime = null;
+
+      for (const row of results) {
+        const pump1State = parseInt(row.pump_1);
+        const pump2State = parseInt(row.pump_2);
+        const dateTime = new Date(row.date_time);
+
+        if (prevDateTime) {
+          const timeDifferenceMinutes = (dateTime - prevDateTime) / 60000; // Convert milliseconds to minutes
+
+          if (prevPump1State === 1) {
+            pump1OnTime += timeDifferenceMinutes;
+          }
+
+          if (prevPump2State === 1) {
+            pump2OnTime += timeDifferenceMinutes;
+          }
+
+          if (prevPump1State !== 1 && prevPump2State !== 1) {
+            combinedOfflineTime += timeDifferenceMinutes;
+          }
+        }
+
+        prevPump1State = pump1State;
+        prevPump2State = pump2State;
+        prevDateTime = dateTime;
+      }
+
+      // Calculate the total hours in the given start and end times
+      const totalHours = (end_time - start_time) / 3600000; // Convert milliseconds to hours
+
+      // Calculate the power cut time by subtracting the on-time from total hours
+      const powerCutTime = totalHours - (pump1OnTime + pump2OnTime + combinedOfflineTime);
+
+      return res.json({
+        pump1OnTime,
+        pump2OnTime,
+        combinedOfflineTime,
+        powerCutTime,
+      });
     });
   } catch (error) {
     console.error('An error occurred:', error);
     res.status(500).json({ message: 'Internal server error' });
   }
 }
+
+
 
 module.exports = {
 	addDevice,
